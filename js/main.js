22328fd0b8d07f73b74b601d15d05e85d5f3dea6
vyakhirev-d <69190976+vyakhirev-d@users.noreply.github.com>
2021-07-24 10:39:51 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
   
-  $(.photo__slider).slick({
+  $('.photo__slider').slick({
     arrows: false,
     prevArrow: '<button> type="button" class="slick-prev"><img src="images/arrow-left.svg"></button>',
     nextArrow: '<button> type="button" class="slick-next"><img src="images/arrow-right.svg"></button>'
