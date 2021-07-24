d112fbe6484a8ee74a493cbb23222633261c9b97
vyakhirev-d <69190976+vyakhirev-d@users.noreply.github.com>
2021-07-24 10:08:45 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  
+  $(.photo__slider).slick({
+    arrows: false,
+    prevArrow: '<button> type="button" class="slick-prev"><img src="images/arrow-left.svg"></button>',
+    nextArrow: '<button> type="button" class="slick-next"><img src="images/arrow-right.svg"></button>'
+  });
+  
 });
